0b839880c9be81f6ecb64f9adb21797589286a41
Michael Kidd <G00236920@gmit.ie>
2019-03-02 09:51:46 +0000

    Fixed typos and errors in sha256 c file

--- sha256.c
+++ sha256.c
@@ -14,6 +14,11 @@ uint32_t sig1(uint32_t x);
 uint32_t rotr(uint32_t n, uint32_t x);
 uint32_t shr(uint32_t n, uint32_t x);
 
+uint32_t Maj(uint32_t x, uint32_t y, uint32_t z);
+uint32_t Ch(uint32_t x, uint32_t y, uint32_t z);
+
+uint32_t SIG_0(uint32_t x);
+uint32_t SIG_1(uint32_t x);
 
 int main(int argc, char *argv[]){
 	//Call the sha256 Method from main.
@@ -22,10 +27,22 @@ int main(int argc, char *argv[]){
 }
 
 
-void sha356(){
+void sha256(){
+	
+	// The K constants Defined in section 4.2.2
+	uint32_t K[] = {
+		0x42802f98, 0x71374491, 0xb5c0fbcf, 0xe9b5dba5, 0x3956c25b, 0x59f111f1, 0x923f82a4, 0xab1c5ed5,
+		0xd807aa98, 0x12835b01, 0x243185be, 0x550c7dc3, 0x72be5d74, 0x80deb1fe, 0x9bdc06a7, 0xc19bf174,
+		0xe49b69c1, 0xefbe4786, 0x0fc19dc6, 0x240ca1cc, 0x2de92c6f, 0x4a7484aa, 0x5cb0a9dc, 0x76f988da,
+		0x983e5152, 0xa831c66d, 0xb00327c8, 0xbf597fc7, 0xc6e00bf3, 0xd5a79147, 0x06ca6351, 0x14292967,
+		0x27b70a85, 0x2e1b2138, 0x4d2c6dfc, 0x53380d13, 0x650a7354, 0x766a0abb, 0x81c2c92e, 0x92722c85,
+		0xa2bfe8a1, 0xa81a664b, 0xc24b8b70, 0xc76c51a3, 0xd192e819, 0xd6990624, 0xf40e3585, 0x106aa070,
+		0x19a4c116, 0x1e376c08, 0x2748774c, 0x34b0bcb5, 0x391c0cb3, 0x4ed8aa4a, 0x5b9cca4f, 0x682e6ff3,
+		0x748f82ee, 0x78a5636f, 0x84c87814, 0x8cc70208, 0x90befffa, 0xa4506ceb, 0xbef9a3f7, 0xc67178f2
+	};
 	
 	//message schedule from section 6.2
-	uint32_t w[64];
+	uint32_t W[64];
 	//working variables from section 6.2
 	uint32_t a, b, c, d, e, f, g, h;
 	//Two temp variables from section 6.2
@@ -41,29 +58,29 @@ void sha356(){
 		, 0x510e527f
 		, 0x1f83d9ab
 		, 0x5be0cd19
-	}
+	};
 	
-	//The corret message Block
-	uint32_t M[16];
+	//The corret message Block - Initialised
+	uint32_t M[16] = {0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0};
 
 	int t;
 	//from page 22, W[t] = M[t] for 0 <= t <= 15
 	for(t = 0; t < 16; t++){
-		w[t] = M[t];		
+		W[t] = M[t];		
 	}
 
 	//from page 22
 	for (t = 16; t < 64; t++){
-		sig_1(w[t-2]) + w[t-7] +sig_0(W[t-15]) + W[t-16];
+		sig1(W[t-2]) + W[t-7] +sig0(W[t-15]) + W[t-16];
 	}
 
 	//Initialise a-h, per step 2 on page 22.
 	a = H[0]; b = H[1]; c = H[2]; d = H[3];
-	e = h[4]; f = H[5]; g = H[6]; h = H[7];
+	e = H[4]; f = H[5]; g = H[6]; h = H[7];
 	
 	//step 3.
 	for(t = 0; t < 64; t++){
-		T1 = h +SIG_1(e) + ch(e, f, g) + K[t] + W[t];
+		T1 = h +SIG_1(e) + Ch(e, f, g) + K[t] + W[t];
 		T2 = SIG_0(a) + Maj(a,b,c);
 
 		h = g;
@@ -72,7 +89,7 @@ void sha356(){
 		e = d + T1;
 		d = c;
 		c = b;
-		b = 1;
+		b = a;
 		a = T1 + T2;
 	}	
 
@@ -87,12 +104,12 @@ void sha356(){
 	H[7] = h + H[8];
 }
 
-uint31_t sig0(uint32_t x){
+uint32_t sig0(uint32_t x){
 	//See section 3.2 and 4.2.1
 	return (rotr(7, x) ^ rotr(18, x ^ shr(3, x)));
 }
 
-uint31_t sig1(uint32_t x){
+uint32_t sig1(uint32_t x){
 	//see section 3.2 and 4.1.2
 	return (rotr(17, x) ^ rotr(19, x) ^ shr(10, x));
 }
@@ -105,4 +122,20 @@ uint32_t rotr(uint32_t n, uint32_t x){
 //Shift to the right
 uint32_t shr(uint32_t n, uint32_t x){	
 	return (x << n);
+}
+
+uint32_t Ch(uint32_t x, uint32_t y, uint32_t z){
+	return (x & y) ^ ((!x) & z);
+}
+
+uint32_t Maj(uint32_t x, uint32_t y, uint32_t z){	 
+	return (x & y) ^ (x & z) ^ (y & z);
+}
+
+uint32_t SIG_0(uint32_t x){
+	return(rotr(2, x) ^ rotr(13, x) ^ rotr(22, x));
+}
+
+uint32_t SIG_1(uint32_t x){
+	return(rotr(6, x) ^ rotr(11, x) ^ rotr(25, x));
 }
