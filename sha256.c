902b6e32e3a8a876c331290d7e1523c49031023e
Michael Kidd <G00236920@gmit.ie>
2019-03-27 16:31:24 +0000

    Program almost done, just need to fix some pointer issues

--- sha256.c
+++ sha256.c
@@ -6,8 +6,14 @@
 #include <stdint.h>
 #include <stdlib.h>
 
+union msgblock{
+	uint8_t e[64];
+	uint32_t t[16];
+	uint64_t s[8];
+};
+
 void mainMenu(char *argv[]);
-void sha256();
+void sha256(FILE *f);
 
 //section 4.2.1
 uint32_t sig0(uint32_t x);
@@ -22,6 +28,11 @@ uint32_t Ch(uint32_t x, uint32_t y, uint32_t z);
 uint32_t SIG_0(uint32_t x);
 uint32_t SIG_1(uint32_t x);
 
+//flag for position within the file being read
+enum status {READ, PAD0, PAD1, FINISH};
+
+int nextmsgblock(FILE *file, union msgblock *M, enum status *S, int *nobits);
+
 int main(int argc, char *argv[]){
 	
 	mainMenu(argv);
@@ -30,6 +41,8 @@ int main(int argc, char *argv[]){
 }
 
 void mainMenu(char *argv[]){
+	
+	FILE *f;
 	int input = 0;
 
 	printf("\n1.\tFile\n");
@@ -37,11 +50,17 @@ void mainMenu(char *argv[]){
 	printf("3.\tExit\n");
 	printf("Choose an Option: ");
 	scanf("%d", &input);
-
+	
 	switch(input){
 	case 1:
-		//Read in a file and hash the file.
-		readFile(argv);
+		//open file given from console
+		f = fopen(argv[1], "r");
+		//Should error check
+		//run the secure hash algorithm
+		sha256(f);
+		//close the file
+		fclose(f);
+		//start program code here
 		mainMenu(argv);
 		break;
 	case 2:
@@ -58,8 +77,15 @@ void mainMenu(char *argv[]){
 	}
 }
 
-void sha256(){
+void sha256(FILE *file){
 	
+	//The current message block
+	union msgblock M;
+	//number of bits read from file
+	uint64_t nobits = 0;
+	//status of the message blocks, in terms of padding
+	enum status S = READ;
+
 	// The K constants Defined in section 4.2.2
 	uint32_t K[] = {
 		0x42802f98, 0x71374491, 0xb5c0fbcf, 0xe9b5dba5, 0x3956c25b, 0x59f111f1, 0x923f82a4, 0xab1c5ed5,
@@ -90,49 +116,117 @@ void sha256(){
 		, 0x1f83d9ab
 		, 0x5be0cd19
 	};
-	
-	//The corret message Block - Initialised
-	uint32_t M[16] = {0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0};
 
 	int t;
-	//from page 22, W[t] = M[t] for 0 <= t <= 15
-	for(t = 0; t < 16; t++){
-		W[t] = M[t];		
-	}
 
-	//from page 22
-	for (t = 16; t < 64; t++){
-		sig1(W[t-2]) + W[t-7] +sig0(W[t-15]) + W[t-16];
-	}
-
-	//Initialise a-h, per step 2 on page 22.
-	a = H[0]; b = H[1]; c = H[2]; d = H[3];
-	e = H[4]; f = H[5]; g = H[6]; h = H[7];
+	while (nextmsgblock(file, *M, *S, *nobits)){
 	
-	//step 3.
-	for(t = 0; t < 64; t++){
-		T1 = h +SIG_1(e) + Ch(e, f, g) + K[t] + W[t];
-		T2 = SIG_0(a) + Maj(a,b,c);
+		//from page 22, W[t] = M[t] for 0 <= t <= 15
+		for(t = 0; t < 16; t++){
+			W[t] = M.t[t];		
+		}
 
-		h = g;
-		g = f;
-		f = e;
-		e = d + T1;
-		d = c;
-		c = b;
-		b = a;
-		a = T1 + T2;
-	}	
+		//from page 22
+		for (t = 16; t < 64; t++){
+			sig1(W[t-2]) + W[t-7] +sig0(W[t-15]) + W[t-16];
+		}
 
-	//step 4
-	H[0] = a + H[0];
-	H[1] = b + H[2];
-	H[2] = c + H[3];
-	H[3] = d + H[4];
-	H[4] = e + H[5];
-	H[5] = f + H[6];
-	H[6] = g + H[7];
-	H[7] = h + H[8];
+		//Initialise a-h, per step 2 on page 22.
+		a = H[0]; b = H[1]; c = H[2]; d = H[3];
+		e = H[4]; f = H[5]; g = H[6]; h = H[7];
+	
+		//step 3.
+		for(t = 0; t < 64; t++){
+			T1 = h +SIG_1(e) + Ch(e, f, g) + K[t] + W[t];
+			T2 = SIG_0(a) + Maj(a,b,c);
+
+			h = g;
+			g = f;
+			f = e;
+			e = d + T1;
+			d = c;
+			c = b;
+			b = a;
+			a = T1 + T2;
+		}	
+
+		//step 4
+		H[0] = a + H[0];
+		H[1] = b + H[2];
+		H[2] = c + H[3];
+		H[3] = d + H[4];
+		H[4] = e + H[5];
+		H[5] = f + H[6];
+		H[6] = g + H[7];
+		H[7] = h + H[8];
+	}
+}
+
+int nextmsgblock(FILE *f, union msgblock *M, enum status *S, int *nobits){
+
+	//Number of bytes we get from file
+	uint64_t nobytes;
+
+	int i;
+	//if all messages blocks done
+	if (*S == FINISH)
+		return 0;
+
+	//Check if we need another block of full padding
+	if(*S == PAD0 || *S == PAD1){
+		//set the first 56 bytes to zero bits
+		for(i = 0; i < 56; i++){
+			M->e[i] = 0x00;
+		}
+		//set the last 64 bits to the number of the bits in the file, - Big endian
+		M->s[7] = *nobits;
+		//tell the status we are finished
+		*S = FINISH;
+	
+		//Set the first bit of M to 1
+		if (*S == PAD1){
+			M->e[0] = 0x80;
+		}
+		//Keep the loop going for another iteration
+		return 1;
+	}
+	
+	//if we get down here, then we still havent finished reading the file
+	nobytes = fread(M->e, 1, 64, f);
+	//Keep track of the number of bytes we have read
+	nobits += (nobytes * 8);
+	//If we read less than 56 bytes, we can put all padding in this block
+	if(nobytes < 56){
+		M->e[nobytes] = 0x80;
+		//get the last 8 bytes
+		while(nobytes < 56){
+			nobytes += 1;
+			//set all bytes to 0
+			M->e[nobytes] = 0x00;
+		}
+		//Append the file size in bits as a (big endian) unsigned 64 bit int
+		M->s[7] = *nobits;
+		*S = FINISH;
+	}
+	//otherwise, check if we can put some padding in this block
+	else if (nobytes < 64){
+		//tell S we need one more message block with padding but wont have the 1 bit.
+		*S = PAD0;
+		//put the one bit in this block
+		M->e[nobytes]  = 0x80;
+		//pad the rest of the block with zero bits	
+		while(nobytes < 64){
+			nobytes = nobytes +1;
+			M->e[nobytes] = 0x00;
+		}
+	//otherwise check if we are at the end of the file	
+	} else if(feof(f)){
+		//tell S that we need a message block with all the padding.
+		*S = PAD1;
+	}
+	
+	//if we get this far, then we will return 1, so that the function will be called again
+	return 1;
 }
 
 uint32_t sig0(uint32_t x){
